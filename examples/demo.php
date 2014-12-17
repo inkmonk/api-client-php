f009f45fd6786005f9e0ef52291ba240a01bc91f
SuryaSankar <suryashankar.m@gmail.com>
2014-12-17 05:34:03 +0530

    claim create finished

--- examples/demo.php
+++ examples/demo.php
@@ -4,13 +4,59 @@ include('../inkmonk.php');
 
 Inkmonk::init();
 
-$shipment = Shipment::get('1');
+/*$customer = Inkmonk_Customer::create(array(
+	"name" => "Harrison Ford",
+	"email" => "hford1@blah.com",
+	"address1" => "8, Seashell Apt",
+	"address2" => "Kenneth Lane",
+	"city" => "Palo Alto",
+	"state" => "California",
+	"country" => "US",
+	"postal_code" => "10008",
+	"contact_number" => "1-34-9008889"
+	));
+var_dump($customer);*/
 
-$merchandise = Merchandise::all();
+$new_customer_params = array(
+	"name" => "Virat Kohli",
+	"email" => "vkohli@blah.com",
+	"address1" => "9, Seashell Apt",
+	"address2" => "Kenneth Lane",
+	"city" => "Palo Alto",
+	"state" => "California",
+	"country" => "US",
+	"postal_code" => "10008",
+	"contact_number" => "1-34-9008889"
+	);
+
+
+$merchandise = Inkmonk_Merchandise::all();
+$m1 = $merchandise[0];
+$m2 = $merchandise[1];
+
+
+$claims = Inkmonk_Claim::create(array(
+	"customers" => array("hford1@blah.com", $new_customer_params ),
+	"slots" => array(
+		array($m1, 1),
+		array($m2->skus, 2)
+		),
+	"form_title" => "Claim Awesome Gifts"
+));
+
+//var_dump($claims);
+foreach($claims as $claim){
+	var_dump($claim->url);
+}
+
+
+/*$shipment = Inkmonk_Shipment::get('1');
+
+$merchandise = Inkmonk_Merchandise::all();
 
 
 var_dump($shipment->address1);
 
 foreach($merchandise as $merch){
 	var_dump($merch->name);
-}
+}*/
--- inkmonk.php
+++ inkmonk.php
@@ -2,15 +2,18 @@
 
 require_once 'lib/Requests/library/Requests.php';
 
+
+
 class Inkmonk{
 
 	private static $version;
 	private static $url;
 	private static $key;
 	private static $secret;
+	public static $site;
 	private static $mime_type = 'application/json';
 
-	private static function get_signed_authorization_header($public_key, $private_key, $message){
+	public static function get_signed_authorization_header($public_key, $private_key, $message){
 		return base64_encode($public_key.':'.hash_hmac(
 			'sha1', $message, $private_key, FALSE));
 	}
@@ -22,21 +25,38 @@ class Inkmonk{
 		self::$url = $ini['API_URL'];
 		self::$key = $ini['API_KEY'];
 		self::$secret = $ini['API_SECRET'];
+		self::$site = $ini['SITE_URL'];
 	}
 
-	static function request($method, $resource){
+	static function request($method, $resource, $data=array()){
 		$url = self::$url.'/'.self::$version.'/'.$resource;
 		$headers = array(
 			'Content-Type' => self::$mime_type,
 			'Authorization' => self::get_signed_authorization_header(
 				  self::$key, self::$secret,
-				  "GET".":".self::$version."/".$resource.":".self::$mime_type));
-		$response = Requests::get($url, $headers);
-		return json_decode($response->body, true);
+				  $method.":".self::$version."/".$resource.":".self::$mime_type));
+		if($method == 'GET'){
+			$response = Requests::get($url, $headers);	
+		}elseif($method == 'POST'){
+			$response = Requests::post($url, $headers, json_encode($data));
+		}
+		else{
+			throw new Exception("Unsupported Request Method");
+		}
+
+		if($response->success){
+			return json_decode($response->body, true);	
+		}else{
+			throw new Exception("Got Exception: ".$response->body["error"]);
+		}
 	}
 
 	static function all($resource){
 		return self::request('GET', $resource);
+	}
+
+	static function create($resource, $data){
+		return self::request('POST', $resource, $data);
 	}
 
 	static function get($resource, $identifier){
@@ -45,11 +65,15 @@ class Inkmonk{
 
 }
 
-abstract class Resource{
+abstract class Inkmonk_Resource{
+
+	static function is_assoc($array) {
+	  return (bool)count(array_filter(array_keys($array), 'is_string'));
+	}
 
 	static protected $resource;
 
-	protected function __construct(array $params = array()){
+	function __construct(array $params = array()){
 		foreach($params as $attr=>$value){
 			$this->$attr = $value;
 		}
@@ -72,25 +96,74 @@ abstract class Resource{
 		if($response['status']=='success')
 			return new static($response['result']);
 	}
+
+	static function create($data){
+		$response = Inkmonk::create(static::$resource, $data);
+		if($response['status']=='success'){
+			if(self::is_assoc($response['result'])){
+				return new static($response['result']);
+			}
+			else{
+				$result=array();
+				foreach($response['result'] as $item){
+					$result[] = new static($item);
+				}
+				return $result;
+			}
+		}
+	}
 }
 
-class Merchandise extends Resource{
+class Inkmonk_Merchandise extends Inkmonk_Resource{
 	static protected $resource = 'merchandise';
+
+	function __construct(array $params = array()){
+		parent::__construct($params);
+		$this->skus = array();
+		foreach($params["skus"] as $sku){
+			array_push($this->skus, new Inkmonk_SKU($sku));
+		}
+	}
 }
 
-class Sku extends Resource{
+class Inkmonk_SKU extends Inkmonk_Resource{
 	static protected $resource = 'skus';
 }
 
-class Customer extends Resource{
+class Inkmonk_Customer extends Inkmonk_Resource{
 	static protected $resource = 'customers';
 }
 
-class Shipment extends Resource{
+class Inkmonk_Shipment extends Inkmonk_Resource{
 	static protected $resource = 'shipments';
 }
 
-class Claim extends Resource{
+class Inkmonk_Claim extends Inkmonk_Resource{
 	static protected $resource = 'claims';
+
+	function __construct(array $params = array()){
+		parent::__construct($params);
+		$this->url = Inkmonk::$site.$this->url;
+	}
+
+	static function create($data){
+		for($j=0; $j<count($data['slots']); $j++){
+			if($data['slots'][$j][0] instanceof Inkmonk_Merchandise){
+				$sku_ids=array();
+				foreach($data['slots'][$j][0]->skus as $sku){
+					$sku_ids[] = $sku->id;
+				}
+				$data['slots'][$j][0] = $sku_ids;
+			}
+			else{
+				for($i=0; $i<count($data['slots'][$j][0]); $i++){
+					if($data['slots'][$j][0][$i] instanceof Inkmonk_SKU){
+						$data['slots'][$j][0][$i] = $data['slots'][$j][0][$i]->id;
+					}
+				}
+			}
+		}
+		return parent::create($data);	
+	}
 }
 
